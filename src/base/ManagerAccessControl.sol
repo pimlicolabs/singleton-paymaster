3aa86cffa93634b7d8c432cd445080c14b2cb416
Garvit Khatri <garvitdelhi@gmail.com>
2025-02-18 19:05:21 +0530

    remove unused imports

--- src/base/ManagerAccessControl.sol
+++ src/base/ManagerAccessControl.sol
@@ -4,9 +4,6 @@ pragma solidity ^0.8.0;
 /* solhint-disable reason-string */
 
 import { AccessControl } from "@openzeppelin-v5.0.2/contracts/access/AccessControl.sol";
-import { IAccessControl } from "@openzeppelin-v5.0.2/contracts/access/IAccessControl.sol";
-import { IERC165 } from "@openzeppelin-v5.0.2/contracts/utils/introspection/IERC165.sol";
-import { IEntryPoint } from "@account-abstraction-v7/interfaces/IEntryPoint.sol";
 
 /**
  * Helper class for creating a contract with multiple valid signers.
