19f6c0057b4e6c4f92e1226e2e59d27b6029516f
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2025-03-12 21:55:49 +0000

    add missing checks

--- src/deployer.sol
+++ src/deployer.sol
@@ -23,28 +23,36 @@ contract PaymasterDeployer {
         address _deterministicDeployer,
         bytes32 _saltV6,
         bytes32 _saltV7,
-        address _entryPoint,
+        address _entryPointV6,
+        address _entryPointV7,
         address _owner,
         address _manager,
         address[] memory _signers
     )
         external
     {
+        // Frontrun protection
         bytes32 messageHash = MessageHashUtils.toEthSignedMessageHash(keccak256(abi.encode(block.chainid)));
         address recoveredSigner = ECDSA.recover(messageHash, _proof);
         require(recoveredSigner == SIGNER, "Invalid proof signature");
 
-        bytes memory initCodeV6 = abi.encodePacked(
-            type(SingletonPaymasterV6).creationCode, abi.encode(_entryPoint, address(this), _manager, _signers)
-        );
+        // Temp signers
+        address[] memory tempSigners = new address[](0);
 
+        // Use temporary initcode
+        bytes memory initCodeV6 = abi.encodePacked(
+            type(SingletonPaymasterV6).creationCode,
+            abi.encode(_entryPointV6, address(this), address(this), tempSigners)
+        );
         bytes memory initCodeV7 = abi.encodePacked(
-            type(SingletonPaymasterV7).creationCode, abi.encode(_entryPoint, address(this), _manager, _signers)
+            type(SingletonPaymasterV7).creationCode,
+            abi.encode(_entryPointV7, address(this), address(this), tempSigners)
         );
 
         bytes memory deployBytecodeV6 = abi.encodePacked(_saltV6, initCodeV6);
         bytes memory deployBytecodeV7 = abi.encodePacked(_saltV7, initCodeV7);
 
+        // Deploy using deterministic deployer
         (bool successV6, bytes memory returnDataV6) = _deterministicDeployer.call(deployBytecodeV6);
         require(successV6, "Failed to deploy SingletonPaymasterV6");
         singletonPaymasterV6 = abi.decode(returnDataV6, (address));
@@ -53,11 +61,25 @@ contract PaymasterDeployer {
         require(successV7, "Failed to deploy SingletonPaymasterV7");
         singletonPaymasterV7 = abi.decode(returnDataV7, (address));
 
+        // Transfer ownership
         SingletonPaymasterV6(singletonPaymasterV6).grantRole(DEFAULT_ADMIN_ROLE, _owner);
         SingletonPaymasterV6(singletonPaymasterV6).revokeRole(DEFAULT_ADMIN_ROLE, address(this));
 
         SingletonPaymasterV7(singletonPaymasterV7).grantRole(DEFAULT_ADMIN_ROLE, _owner);
         SingletonPaymasterV7(singletonPaymasterV7).revokeRole(DEFAULT_ADMIN_ROLE, address(this));
+
+        // Transfer manager role
+        SingletonPaymasterV6(singletonPaymasterV6).grantRole(MANAGER_ROLE, _manager);
+        SingletonPaymasterV6(singletonPaymasterV6).revokeRole(MANAGER_ROLE, address(this));
+
+        SingletonPaymasterV7(singletonPaymasterV7).grantRole(MANAGER_ROLE, _manager);
+        SingletonPaymasterV7(singletonPaymasterV7).revokeRole(MANAGER_ROLE, address(this));
+
+        // Add signers
+        for (uint256 i = 0; i < _signers.length; i++) {
+            SingletonPaymasterV6(singletonPaymasterV6).addSigner(_signers[i]);
+            SingletonPaymasterV7(singletonPaymasterV7).addSigner(_signers[i]);
+        }
     }
 }
 
@@ -69,7 +91,8 @@ contract DeployerFactory {
         address _deterministicDeployer,
         bytes32 _saltV6,
         bytes32 _saltV7,
-        address _entryPoint,
+        address _entryPointV6,
+        address _entryPointV7,
         address _owner,
         address _manager,
         address[] memory _signers
@@ -78,7 +101,7 @@ contract DeployerFactory {
         paymasterDeployer = address(deployer);
 
         deployer.deployPaymasters(
-            _proof, _deterministicDeployer, _saltV6, _saltV7, _entryPoint, _owner, _manager, _signers
+            _proof, _deterministicDeployer, _saltV6, _saltV7, _entryPointV6, _entryPointV7, _owner, _manager, _signers
         );
     }
 }
