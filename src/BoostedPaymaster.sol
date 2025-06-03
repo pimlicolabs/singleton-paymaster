9c1a2c3b42b7180cfdffd1e0e56599af113cfadb
Garvit Khatri <garvitdelhi@gmail.com>
2025-06-03 14:09:05 +0100

    remove base paymaster

--- src/BoostedPaymaster.sol
+++ src/BoostedPaymaster.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import { BaseSingletonPaymaster } from "./base/BaseSingletonPaymaster.sol";
 import { UserOperation } from "@account-abstraction-v6/interfaces/IPaymaster.sol";
 import { PackedUserOperation } from "@account-abstraction-v7/interfaces/PackedUserOperation.sol";
 import { UserOperationLib } from "@account-abstraction-v7/core/UserOperationLib.sol";
@@ -9,22 +8,26 @@ import { _packValidationData } from "@account-abstraction-v6/core/Helpers.sol";
 
 using UserOperationLib for PackedUserOperation;
 
-contract BoostedPaymaster is BaseSingletonPaymaster {
-    /**
-     * @notice Initializes a BoosterPaymaster instance.
-     * @param _entryPoint The entryPoint address.
-     * @param _owner The initial contract owner.
-     * @param _manager The manager address.
-     * @param _signers Array of signer addresses.
-     */
-    constructor(
-        address _entryPoint,
-        address _owner,
-        address _manager,
-        address[] memory _signers
-    )
-        BaseSingletonPaymaster(_entryPoint, _owner, _manager, _signers)
-    { }
+contract BoostedPaymaster {
+    /// @notice Mode indicating that the Paymaster is in Verifying mode.
+    uint8 immutable VERIFYING_MODE = 0;
+
+    /// @dev Emitted when a user operation is sponsored by the paymaster.
+    event UserOperationSponsored(
+        bytes32 indexed userOpHash,
+        /// @param The user that requested sponsorship.
+        address indexed user,
+        /// @param The paymaster mode that was used.
+        uint8 paymasterMode,
+        /// @param The token that was used during sponsorship (ERC-20 mode only).
+        address token,
+        /// @param The amount of token paid during sponsorship (ERC-20 mode only).
+        uint256 tokenAmountPaid,
+        /// @param The exchange rate of the token at time of sponsorship (ERC-20 mode only).
+        uint256 exchangeRate
+    );
+
+    constructor() { }
 
     /**
      * @notice Validates a UserOperation (v6 structure) for paymaster usage.
