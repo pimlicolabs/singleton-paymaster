c7f23d69596271c962e2abe4a4240682fbdd30b0
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2025-06-03 14:25:45 +0100

    use BaseSingletonPaymaster event

--- src/BoostedPaymaster.sol
+++ src/BoostedPaymaster.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import { UserOperation } from "@account-abstraction-v6/interfaces/IPaymaster.sol";
-import { PackedUserOperation } from "@account-abstraction-v7/interfaces/PackedUserOperation.sol";
-import { UserOperationLib } from "@account-abstraction-v7/core/UserOperationLib.sol";
-import { _packValidationData } from "@account-abstraction-v6/core/Helpers.sol";
+import {UserOperation} from "@account-abstraction-v6/interfaces/IPaymaster.sol";
+import {PackedUserOperation} from "@account-abstraction-v7/interfaces/PackedUserOperation.sol";
+import {UserOperationLib} from "@account-abstraction-v7/core/UserOperationLib.sol";
+import {BaseSingletonPaymaster} from "./base/BaseSingletonPaymaster.sol";
+import {_packValidationData} from "@account-abstraction-v6/core/Helpers.sol";
 
 using UserOperationLib for PackedUserOperation;
 
@@ -12,22 +13,7 @@ contract BoostedPaymaster {
     /// @notice Mode indicating that the Paymaster is in Verifying mode.
     uint8 immutable VERIFYING_MODE = 0;
 
-    /// @dev Emitted when a user operation is sponsored by the paymaster.
-    event UserOperationSponsored(
-        bytes32 indexed userOpHash,
-        /// @param The user that requested sponsorship.
-        address indexed user,
-        /// @param The paymaster mode that was used.
-        uint8 paymasterMode,
-        /// @param The token that was used during sponsorship (ERC-20 mode only).
-        address token,
-        /// @param The amount of token paid during sponsorship (ERC-20 mode only).
-        uint256 tokenAmountPaid,
-        /// @param The exchange rate of the token at time of sponsorship (ERC-20 mode only).
-        uint256 exchangeRate
-    );
-
-    constructor() { }
+    constructor() {}
 
     /**
      * @notice Validates a UserOperation (v6 structure) for paymaster usage.
@@ -36,16 +22,11 @@ contract BoostedPaymaster {
      * @return context The context to be passed to postOp.
      * @return validationData The validation data indicating success/failure.
      */
-    function validatePaymasterUserOp(
-        UserOperation calldata userOp,
-        bytes32 userOpHash,
-        uint256
-    )
+    function validatePaymasterUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256)
         external
         returns (bytes memory context, uint256 validationData)
     {
-        // Emit the serOperationSponsored event
-        emit UserOperationSponsored(userOpHash, userOp.sender, VERIFYING_MODE, address(0), 0, 0);
+        emit BaseSingletonPaymaster.UserOperationSponsored(userOpHash, userOp.sender, VERIFYING_MODE, address(0), 0, 0);
 
         validationData = _packValidationData(false, 0, 0);
 
@@ -59,15 +40,13 @@ contract BoostedPaymaster {
      * @return context The context to be passed to postOp.
      * @return validationData The validation data indicating success/failure.
      */
-    function validatePaymasterUserOp(
-        PackedUserOperation calldata userOp,
-        bytes32 userOpHash,
-        uint256
-    )
+    function validatePaymasterUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256)
         external
         returns (bytes memory context, uint256 validationData)
     {
-        emit UserOperationSponsored(userOpHash, userOp.getSender(), VERIFYING_MODE, address(0), 0, 0);
+        emit BaseSingletonPaymaster.UserOperationSponsored(
+            userOpHash, userOp.getSender(), VERIFYING_MODE, address(0), 0, 0
+        );
 
         validationData = _packValidationData(false, 0, 0);
 
